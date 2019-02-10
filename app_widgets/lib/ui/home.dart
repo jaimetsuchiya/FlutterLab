ad061875f333bffdcffebf529e0271cb90be95e8
Tsuchiya <Jaime.Tsuchiya@la.imtn.com>
2019-02-10 16:43:22 -0200

    Lab Flutter

---
+++ app_widgets/lib/ui/home.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget
+{
+  @override
+  Widget build(BuildContext context) {
+    
+    
+    return Container(
+      alignment: Alignment.center,
+      color: Colors.deepOrangeAccent,
+      child: Text("Pizza", textDirection: TextDirection.ltr,)
+    );
+  }
+  
+}
